564578815d844ec0aae5d3eec3e5ba01340add6d
zgc <zgc8895@gmail.com>
2025-06-28 21:48:40 +0800

    Change baseUrl to real ip

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:32771',
+    baseUrl: 'http://192.168.1.10:32771',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
