c6dc28561d0925ef69c0538e69d692305a216400
zgc <zgc8895@gmail.com>
2025-06-28 14:59:52 +0800

    Restore default reporter

--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,4 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-  reporter: 'junit',
-  reporterOptions: {
-    mochaFile: 'results/my-test-output.xml',
-    toConsole: true,
-  },
 });
